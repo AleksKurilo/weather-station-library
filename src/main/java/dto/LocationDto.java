6157c540fc8667fd8c428b447596b9d90ffefa29
AleksKurilo <akurilo@outlook.com>
2018-11-27 11:53:04 +0200

    refactoring

--- src/main/java/dto/LocationDto.java
+++ src/main/java/dto/LocationDto.java
@@ -2,10 +2,13 @@ package dto;
 
 import lombok.Data;
 
+import java.util.Set;
+
 
 @Data
 public class LocationDto extends BaseEntityDto {
 
     private String name;
     private String coordinates;
+    private Set<StationDto> stations;
 }

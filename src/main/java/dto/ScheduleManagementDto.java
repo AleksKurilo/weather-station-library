94ffed96ccf684bed5fc3574f8667f120d76b686
aleksk <aleks_prime@mail.ru>
2019-05-14 07:51:34 +0300

    Changed the place where the schedule actor runs (now he starts after  creating spring-bean)

---
+++ src/main/java/dto/ScheduleManagementDto.java
@@ -0,0 +1,10 @@
+package dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class ScheduleManagementDto {
+    private final boolean cancel;
+}
